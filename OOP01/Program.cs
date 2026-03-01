ec898660c6b0dd97a673ec9533646bc32006c9a1
hala daif <daifhala76@gmail.com>
2026-03-01 20:53:33 +0200

    part01

--- OOP01/Program.cs
+++ OOP01/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.ComponentModel;
 using System.Drawing;
 using System.Numerics;
 using System.Runtime.Intrinsics.X86;
@@ -24,15 +25,23 @@ namespace OOP01
             //    Class >>Reference type , Stored In Heap , Copies the reference , Complex entities with behavior
             //example>>
             //class Student
-    //public string Name;
-    //    public int Age;
+            //public string Name;
+            //    public int Age;
 
-    //    public void Introduce()
-    //    {
-    //        Console.WriteLine($"Hi, I'm {Name}, age {Age}");
-    //    }
-    //}
-    #endregion
-}
+            //    public void Introduce()
+            //    {
+            //        Console.WriteLine($"Hi, I'm {Name}, age {Age}");
+            //    }
+            //}
+
+            //Q2 : Explain the difference between public and private access modifiers with an example.
+            //private>>Accessibility: The member is only accessible within the class or struct it is defined in. It cannot be accessed from outside the class.
+            //public>>Accessibility: The member is accessible from anywhere in the application, both within the same assembly (project) and from other assemblies.
+
+            //Q4 : What is a class library? Why do we use class libraries?
+            //A Class Library is a separate project that contains reusable classes, but has no Main method and cannot run on its own.It compiles into a .dll file(Dynamic Link Library).
+            //Reusability ,Organization , Teamwork ,  Maintenance
+            #endregion
+        }
     }
     }
